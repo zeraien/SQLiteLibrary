81adf24103f7d5a39a8ce0313d5d4a747e5107dc
Dmitri Fedortchenko <d@angelhill.net>
2012-04-10 16:03:48 +0200

    first commit

---
+++ SQLiteLibrary.h
@@ -0,0 +1,73 @@
+//
+//  Created by zeraien on 11/11/11.
+//
+//
+
+
+#import <Foundation/Foundation.h>
+#import <sqlite3.h>
+
+#define sqlite_now_epoch @"strftime('%s','now')"
+#define make_nil_if_null(__string__) (__string__==nil||[__string__ isEqualToString:@"(null)"])?nil:__string__
+
+#define sqlite3_to_string(_statement_, __column__) \
+							(char *)sqlite3_column_text(_statement_, __column__)? \
+									make_nil_if_null([NSString stringWithUTF8String:(char *)sqlite3_column_text(_statement_, __column__)]):nil
+
+typedef void (^SQLiteBlock)(sqlite3_stmt *compiledStatement);
+
+@interface SQLiteLibrary : NSObject
+{
+	sqlite3 *database;
+}
++ (SQLiteLibrary *)singleton;
+
++ (void)setDatabaseFile:(NSString *)dbFilePath;
+
++ (void)setDatabaseFileInDocuments:(NSString *)dbFilename;
+
++ (void)setDatabaseFileInCache:(NSString *)dbFilename;
+
+
+/**
+* Begin transaction (singleton edition)
+*/
++ (BOOL)begin;
+
+/**
+* Commit transaction (singleton edition)
+*/
++ (BOOL)commit;
+
+/**
+* See +performQuery:block:
+*/
+- (int64_t)performQueryInTransaction:(NSString *)query block:(SQLiteBlock)block;
+
+/** Perform an SQL query. Works with any SQL query. (singleton edition)
+* If no transaction has been started, the method will start a new transaction and auto-commit at the end of the query.
+*
+* @param query SQL query
+* @param block Block with SQL result
+* */
++ (int64_t)performQuery:(NSString *)query block:(SQLiteBlock)block;
+
+/**
+* Copy database skeleton to user's documents directory.
+* @param forceReset if True, overwrite existing database in user's documents directory
+*/
++ (void)setupDatabaseAndForceReset:(BOOL)forceReset;
+
+/**
+* Begin transaction
+*/
+- (BOOL)begin;
+
+
+/**
+* Commit transaction
+*/
+- (BOOL)commit;
+
+
+@end
---
+++ SQLiteLibrary.m
@@ -0,0 +1,216 @@
+//
+//  Created by zeraien on 11/11/11.
+//
+//
+
+
+#import "SQLiteLibrary.h"
+#import "ODBMacros.h"
+
+
+@implementation SQLiteLibrary
+{
+    NSString* dbFilePath_;
+}
+
++ (void)setDatabaseFileInCache:(NSString *)dbFilename
+{
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES);
+    NSString *directory = [paths objectAtIndex:0];
+    NSString* appFile = [directory stringByAppendingPathComponent:dbFilename];
+    [self setDatabaseFile:appFile];
+
+}
++ (void)setDatabaseFileInDocuments:(NSString *)dbFilename
+{
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *directory = [paths objectAtIndex:0];
+    NSString* appFile = [directory stringByAppendingPathComponent:dbFilename];
+    [self setDatabaseFile:appFile];
+}
++ (void)setDatabaseFile:(NSString *)dbFilePath
+{
+    SQLiteLibrary * me = [self singleton];
+    [me->dbFilePath_ release]; me->dbFilePath_ = nil;
+
+    me->dbFilePath_ = [dbFilePath copy];
+}
+
++ (SQLiteLibrary *)singleton
+{
+	static SQLiteLibrary *_instance = nil;
+
+	@synchronized (self)
+	{
+		if (_instance == nil)
+		{
+			_instance = [[self alloc] init];
+		}
+	}
+
+	return _instance;
+}
+
+- (id)init
+{
+	self = [super init];
+	if (self)
+	{
+		database = nil;
+	}
+
+	return self;
+
+}
+
++ (BOOL)begin
+{
+	return [[self singleton] begin];
+}
+- (BOOL)begin
+{
+    NSAssert(dbFilePath_!=nil, @"dbFilePath must be set!");
+
+	NSString*dbPath = dbFilePath_;
+#if DEBUG_LOG>=2
+	NSLog(@"Using sqlite database at path %@", dbPath);
+#endif
+	NSAssert([[NSFileManager defaultManager] isReadableFileAtPath:dbPath], ODBsprintf(@"Database file does not exist %@", dbPath));
+
+	if (database!=nil)
+		[self commit];
+
+	NSAssert(database==nil, @"Attempted to start transaction while another is in progress.");
+	
+	if(sqlite3_open([dbPath UTF8String], &database) == SQLITE_OK) {
+#if DEBUG_LOG>=2
+		NSLog(@"TRANSACTION BEGIN");
+#endif
+		sqlite3_exec(database, "BEGIN;", NULL, NULL, NULL);
+		if (sqlite3_errcode(database) != SQLITE_DONE && sqlite3_errcode(database)>0)
+		{
+#if DEBUG_LOG>=1
+			NSLog(@"======> SQLITE ERROR ===============> %d - %@", sqlite3_errcode(database), [NSString stringWithCString:sqlite3_errmsg(database) encoding:NSUTF8StringEncoding]);
+#endif
+			return NO;
+		}
+		return YES;
+	}
+#if DEBUG_LOG>=1
+	NSLog(@"======> SQLITE ERROR ===============> Failed to open SQLite database %@;", dbPath);
+#endif
+	return NO;
+}
+
+
+- (int64_t)performQueryInTransaction:(NSString *)query block:(SQLiteBlock)block
+{
+	BOOL shouldCommit = NO;
+	if (database == nil)
+	{
+#if DEBUG_LOG>=1
+		NSLog(@"======> SQLITE WARNING ===============> No transaction started, forcing autocommit");
+#endif
+        shouldCommit=YES;
+		[self begin];
+	}
+	NSAssert(database!=nil, @"Must begin a transaction first.");
+	if (database == nil) return NO;
+
+	int returnValue = -1;
+
+#if DEBUG_LOG>=2
+	NSLog(@"Performing query:\n\t%@", query);
+#endif
+	// Setup the SQL Statement and compile it for faster access
+	sqlite3_stmt *compiledStatement;
+	if(sqlite3_prepare_v2(database, [query UTF8String], -1, &compiledStatement, NULL) == SQLITE_OK) {
+		// Loop through the results and add them to the feeds array
+		[block retain];
+		int resultCount = 0;
+		while(sqlite3_step(compiledStatement) == SQLITE_ROW) {
+			// Read the data from the result row
+			resultCount ++;
+			if (block!=nil)
+				block(compiledStatement);
+		}
+
+#if DEBUG_LOG>=2
+		if (sqlite3_errcode(database) != SQLITE_DONE && sqlite3_errcode(database)>0)
+		{
+			NSLog(@"======> SQLITE ERROR ===============> %d - %@", sqlite3_errcode(database), [NSString stringWithCString:sqlite3_errmsg(database) encoding:NSUTF8StringEncoding]);
+		}
+#endif
+		if ([[query uppercaseString] hasPrefix:@"INSERT"])
+			returnValue = (int)sqlite3_last_insert_rowid(database);
+		else if ([[query uppercaseString] hasPrefix:@"SELECT"])
+			returnValue = resultCount;
+		else
+			returnValue = sqlite3_changes(database);
+
+		[block release];
+	}
+#if DEBUG_LOG>=1
+	else
+	{
+		NSLog(@"======> SQLITE ERROR ===============> %d - %@", sqlite3_errcode(database), [NSString stringWithCString:sqlite3_errmsg(database) encoding:NSUTF8StringEncoding]);
+	}
+#endif
+	// Release the compiled statement from memory
+	sqlite3_finalize(compiledStatement);
+	if (shouldCommit)
+		[self commit];
+	return returnValue;
+}
+
++ (BOOL)commit
+{
+	return [[self singleton] commit];
+}
+
+- (BOOL)commit
+{
+	if (database == nil) return NO;
+
+	sqlite3_exec(database, "COMMIT;", NULL, NULL, NULL);
+	BOOL success = YES;
+	if (sqlite3_errcode(database) != SQLITE_DONE && sqlite3_errcode(database)>0)
+	{
+#if DEBUG_LOG>=1
+		NSLog(@"======> SQLITE ERROR ===============> %d - %@", sqlite3_errcode(database), [NSString stringWithCString:sqlite3_errmsg(database) encoding:NSUTF8StringEncoding]);
+#endif
+        success=NO;
+	}
+#if DEBUG_LOG>=2
+	NSLog(@"TRANSACTION COMMIT");
+#endif
+	sqlite3_close(database);
+	database = nil;
+	return success;
+}
+
++ (int64_t)performQuery:(NSString *)query block:(SQLiteBlock)block
+{
+	return [[self singleton] performQueryInTransaction:query block:block];
+}
+
+- (void)setupDatabaseAndForceReset:(BOOL)forceReset
+{
+    NSAssert(dbFilePath_!=nil, @"dbFilePath must be set!");
+
+    NSString* defaultDB = [[NSBundle mainBundle]pathForResource:@"data_skeleton" ofType:@"sqlite3"];
+    NSString* appFile = dbFilePath_;
+    BOOL exists = [[NSFileManager defaultManager]fileExistsAtPath:appFile];
+    if (exists && forceReset)
+        [[NSFileManager defaultManager] removeItemAtPath:appFile error:nil];
+
+    if (!exists || forceReset)
+        [[NSFileManager defaultManager]copyItemAtPath:defaultDB toPath:appFile error:nil];
+}
+
++ (void)setupDatabaseAndForceReset:(BOOL)forceReset
+{
+    [[self singleton] setupDatabaseAndForceReset:forceReset];
+}
+
+@end
